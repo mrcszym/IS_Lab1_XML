98a80d8f6947261b2ae880178117d8a23dc63a34
Szymon <marciniecszymon00@gmail.com>
2022-03-05 18:36:46 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace IS_Lab1_XML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string xmlpath = Path.Combine("Assets", "data.xml");
+            // odczyt danych z wykorzystaniem DOM
+            Console.WriteLine("XML loaded by DOM Approach");
+            XMLReadWithDOMApproach.Read(xmlpath);
+            /*
+            // odczyt danych z wykorzystaniem SAX
+            Console.WriteLine("XML loaded by SAX Approach");
+            XMLReadWithSAXApproach.Read(xmlpath);
+            */
+            /*
+            // odczyt danych z wykorzystaniem XPath i DOM
+            Console.WriteLine("XML loaded with XPath");
+            XMLReadWithXLSTDOM.Read(xmlpath);
+            */
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ XMLReadWithDOMApproach.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+
+namespace IS_Lab1_XML
+{
+    internal class XMLReadWithDOMApproach
+    {
+        internal static void Read(string filepath)
+        {
+            // odczyt zawartości dokumentu
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filepath);
+
+            string postac;
+            string sc;
+            int count = 0;
+            var drugs = doc.GetElementsByTagName("produktLeczniczy");
+            foreach (XmlNode d in drugs)
+            {
+                postac = d.Attributes.GetNamedItem("postac").Value;
+                sc =
+                d.Attributes.GetNamedItem("nazwaPowszechnieStosowana").Value;
+                if (postac == "Krem" && sc == "Mometasoni furoas")
+                    count++;
+            }
+            Console.WriteLine("Liczba produktów leczniczych w postaci kremu, których jedyną substancją czynną jest Mometasoni furoas {0}", count);
+
+        }
+    }
+}
